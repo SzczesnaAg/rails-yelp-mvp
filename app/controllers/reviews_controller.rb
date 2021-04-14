61694c673a58037c7bde2a451229ecc5da65b52a
SzczesnaAg <agata.szczesna89@gmail.com>
2021-04-14 17:27:06 +0200

    review stil not working :(

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,6 +18,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:rating, :content)
   end
 end
